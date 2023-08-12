814813a2a6ca1bc2a6028d88d9d1511d1bc75329
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-08-12 21:22:24 +0100

    Forgot Getter and Setter

--- src/image/Image.h
+++ src/image/Image.h
@@ -8,29 +8,32 @@
 /// </summary>
 class Image {
 public:
-    Image(const char* file, const int forceChannels = 0);
-    Image(const Image& other);
-    ~Image();
+	Image(const char* file, const int forceChannels = 0);
+	Image(const Image& other);
+	~Image();
 
-    Image operator=(const Image& other);
+	Image operator=(const Image& other);
 
-    bool Read(const char* file, const int forceChannels = 0);
-    bool Write(const char* file);
+	bool Read(const char* file, const int forceChannels = 0);
+	bool Write(const char* file);
 
-    inline int GetChannels() const { return m_channels; };
-    inline size_t GetSize() const { return m_size; };
+	inline int GetChannels() const { return m_channels; };
+	inline size_t GetSize() const { return m_size; };
+
+	inline uint8_t GetData(const size_t index) const { return m_data[index]; };
+	inline void SetData(const size_t index, const uint8_t data) { m_data[index] = data; };
 
 public:
-    enum class ImageType {
-        PNG, JPG, BMP, TGA, NA
-    };
+	enum class ImageType {
+		PNG, JPG, BMP, TGA, NA
+	};
 
 private:
-    uint8_t* m_data;
-    size_t m_size = 0;
-    int m_w, m_h, m_channels;
+	uint8_t* m_data;
+	size_t m_size = 0;
+	int m_w, m_h, m_channels;
 
-    ImageType GetFileType(const char* file);
+	ImageType GetFileType(const char* file);
 };
 
 #endif // !IMAGE_H
