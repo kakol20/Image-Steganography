c2f43b729ebb117ca3a13bd5d1d4936b2ac29aa3
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-08-12 15:39:00 +0100

    Upload CMake project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿// Image Steganography.cpp : Defines the entry point for the application.
+//
+
+#include "Image Steganography.h"
+
+int main() {
+	std::cout << "Hello CMake.\n";
+
+	std::cin.get();
+	return 0;
+}
