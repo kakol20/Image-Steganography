7ee135b07203f7447888bab4a94ec77df3a1c39a
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-08-15 14:46:08 +0100

    Simple Img2Img steganography

--- src/process/Img2Img.cpp
+++ src/process/Img2Img.cpp
@@ -7,6 +7,13 @@ void Img2Img::Run(const std::string baseImg, const std::string hiddenImg, const 
 	Image hidden(hiddenImg.c_str(), 3);
 
 	Image output = base;
+	Image lsb = output;
+
+	uint8_t bitMask = 0;
+	for (int i = 0; i < (int)significantBits; i++) {
+		bitMask = bitMask | (0b1 << i);
+	}
+	int shiftRight = 8 - significantBits;
 
 	for (int x = 0; x < base.GetWidth(); x++) {
 		if (!repeat && x >= hidden.GetWidth()) break;
@@ -20,16 +27,26 @@ void Img2Img::Run(const std::string baseImg, const std::string hiddenImg, const 
 
 			size_t baseIndex = Img2Img::GetIndex(x, y, base.GetWidth(), 3);
 			size_t hiddenIndex = Img2Img::GetIndex(hiddenX, hiddenY, hidden.GetWidth(), 3);
+			
+			for (size_t i = 0; i < 3; i++) {
+				uint8_t baseData = base.GetData(baseIndex + i) & (~bitMask);
 
-			output.SetData(baseIndex + 0, hidden.GetData(hiddenIndex + 0));
-			output.SetData(baseIndex + 1, hidden.GetData(hiddenIndex + 1));
-			output.SetData(baseIndex + 2, hidden.GetData(hiddenIndex + 2));
+				if (dithered) {
+					// do nothing for now
+					output.SetData(baseIndex + i, hidden.GetData(hiddenIndex + i));
+				}
+				else {
+					uint8_t hiddenData = (hidden.GetData(hiddenIndex + i) >> shiftRight) & bitMask;
+
+					output.SetData(baseIndex + i, baseData | hiddenData);
+					lsb.SetData(baseIndex + i, (255 / bitMask) * hiddenData);
+				}
+			}
 		}
 	}
 
 	output.Write(outputImg.c_str());
 
-	Image lsb = output;
 	lsb.Write(significantBitsImg.c_str());
 }
 
