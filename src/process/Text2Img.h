f54576799fa73e9153463d023bcaa135ae1ace94
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-09-02 18:37:45 +0100

    Separate text file for text2img

--- src/process/Text2Img.h
+++ src/process/Text2Img.h
@@ -7,5 +7,5 @@ public:
 	Text2Img() {};
 	~Text2Img() {};
 
-	static void Run(const std::string in, const std::string out, const std::string significantBitsImg, const int significantBits, const bool repeat, const std::string text);
+	static void Run(const std::string in, const std::string out, const std::string significantBitsImg, const int significantBits, const bool repeat, const std::string textFile);
 };
